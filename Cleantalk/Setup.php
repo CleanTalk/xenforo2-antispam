2ffafcd07a51e3dec4d528ec3de8c0b47e056620
davydovct <davydov@cleantalk.org>
2017-11-08 11:51:06 +0500

    Release 1.1 Added hashes.json Removes unused classes

--- Cleantalk/Setup.php
+++ Cleantalk/Setup.php
@@ -12,39 +12,4 @@ class Setup extends AbstractSetup
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
-	/**
-	 * ----------------
-	 *   INSTALLATION
-	 * ----------------
-	 */
-
-	/* CREATE xf_kl_em_fonts */
-	public function installStep1()
-	{
-
-	}
-	/**
-	 * ----------------
-	 *     UPGRADES
-	 * ----------------
-	 */
-
-	/* 1.0.0 Beta 2*/
-	/* CREATE xf_kl_em_templates */
-	public function upgrade10000Step1() 
-	{
-
-	}
-
-	/**
-	 * ----------------
-	 *  UNINSTALLATION
-	 * ----------------
-	 */
-	
-	/* DROP xf_kl_em_fonts */
-	public function uninstallStep1() 
-	{
-
-	}
 }
--- Cleantalk/XF/Service/Post/Preparer.php
+++ Cleantalk/XF/Service/Post/Preparer.php
@@ -5,127 +5,8 @@ use CleanTalk\Cleantalk;
 use CleanTalk\CleantalkRequest;
 use XF\Entity\Post;
 
-class Preparer extends \XF\Service\AbstractService
+class Preparer extends XFCP_Preparer
 {
-	/**
-	 * @var Post
-	 */
-	protected $post;
-
-	protected $attachmentHash;
-
-	protected $logIp = true;
-
-	protected $quotedPosts = [];
-
-	protected $mentionedUsers = [];
-
-	public function __construct(\XF\App $app, Post $post)
-	{
-		parent::__construct($app);
-		$this->post = $post;
-	}
-
-	public function getPost()
-	{
-		return $this->post;
-	}
-
-	public function logIp($logIp)
-	{
-		$this->logIp = $logIp;
-	}
-
-	public function getQuotedPosts()
-	{
-		return $this->quotedPosts;
-	}
-
-	public function getQuotedUserIds()
-	{
-		if (!$this->quotedPosts)
-		{
-			return [];
-		}
-
-		$postIds = array_keys($this->quotedPosts);
-		$quotedUserIds = [];
-
-		$db = $this->db();
-		$postUserMap = $db->fetchPairs("
-			SELECT post_id, user_id
-			FROM xf_post
-			WHERE post_id IN (" . $db->quote($postIds) .")
-		");
-		foreach ($postUserMap AS $postId => $userId)
-		{
-			if (!isset($this->quotedPosts[$postId]) || !$userId)
-			{
-				continue;
-			}
-
-			$quote = $this->quotedPosts[$postId];
-			if (!isset($quote['member']) || $quote['member'] == $userId)
-			{
-				$quotedUserIds[] = $userId;
-			}
-		}
-
-		return $quotedUserIds;
-	}
-
-	public function getMentionedUsers($limitPermissions = true)
-	{
-		if ($limitPermissions && $this->post)
-		{
-			/** @var \XF\Entity\User $user */
-			$user = $this->post->User ?: $this->repository('XF:User')->getGuestUser();
-			return $user->getAllowedUserMentions($this->mentionedUsers);
-		}
-		else
-		{
-			return $this->mentionedUsers;
-		}
-	}
-
-	public function getMentionedUserIds($limitPermissions = true)
-	{
-		return array_keys($this->getMentionedUsers($limitPermissions));
-	}
-
-	public function setMessage($message, $format = true, $checkValidity = true)
-	{
-		$preparer = $this->getMessagePreparer($format);
-		$this->post->message = $preparer->prepare($message, $checkValidity);
-		$this->post->embed_metadata = $preparer->getEmbedMetadata();
-
-		$this->quotedPosts = $preparer->getQuotesKeyed('post');
-		$this->mentionedUsers = $preparer->getMentionedUsers();
-
-		return $preparer->pushEntityErrorIfInvalid($this->post);
-	}
-
-	/**
-	 * @param bool $format
-	 *
-	 * @return \XF\Service\Message\Preparer
-	 */
-	protected function getMessagePreparer($format = true)
-	{
-		/** @var \XF\Service\Message\Preparer $preparer */
-		$preparer = $this->service('XF:Message\Preparer', 'post');
-		if (!$format)
-		{
-			$preparer->disableAllFilters();
-		}
-
-		return $preparer;
-	}
-
-	public function setAttachmentHash($hash)
-	{
-		$this->attachmentHash = $hash;
-	}
 
 	public function checkForSpam()
 	{
@@ -179,7 +60,7 @@ class Preparer extends \XF\Service\AbstractService
         $ct_request->sender_nickname = $user->username;
         $ct_request->message = $post->Thread->title."\n".$post->message;
         $ct_request->sender_ip = $ct->cleantalk_get_real_ip();
-        $ct_request->agent = 'xenforo2-10';
+        $ct_request->agent = 'xenforo2-11';
         $ct_request->js_on = $js_on;
         $ct_request->submit_time = time() - (int)$session_values['ct_page_start_time'];
         $ct_request->sender_info = $sender_info;
@@ -212,80 +93,6 @@ class Preparer extends \XF\Service\AbstractService
 			case 'cleantalk_spam':
 				$post->error($reason);
 				break;
-		}
-	}
-
-	public function afterInsert()
-	{
-		if ($this->attachmentHash)
-		{
-			$this->associateAttachments($this->attachmentHash);
-		}
-
-		if ($this->logIp)
-		{
-			$ip = ($this->logIp === true ? $this->app->request()->getIp() : $this->logIp);
-			$this->writeIpLog($ip);
-		}
-
-		$post = $this->post;
-		$checker = $this->app->spam()->contentChecker();
-
-		if ($post->isFirstPost())
-		{
-			$checker->logContentSpamCheck('thread', $post->thread_id);
-			$checker->logSpamTrigger('thread', $post->thread_id);
-		}
-		else
-		{
-			$checker->logContentSpamCheck('post', $post->post_id);
-			$checker->logSpamTrigger('post', $post->post_id);
-		}
-	}
-
-	public function afterUpdate()
-	{
-		if ($this->attachmentHash)
-		{
-			$this->associateAttachments($this->attachmentHash);
-		}
-
-		$post = $this->post;
-		$checker = $this->app->spam()->contentChecker();
-
-		if ($post->isFirstPost())
-		{
-			$checker->logSpamTrigger('thread', $post->thread_id);
-		}
-		else
-		{
-			$checker->logSpamTrigger('post', $post->post_id);
-		}
-	}
-
-	protected function associateAttachments($hash)
-	{
-		$post = $this->post;
-
-		/** @var \XF\Service\Attachment\Preparer $inserter */
-		$inserter = $this->service('XF:Attachment\Preparer');
-		$associated = $inserter->associateAttachmentsWithContent($hash, 'post', $post->post_id);
-		if ($associated)
-		{
-			$post->fastUpdate('attach_count', $post->attach_count + $associated);
-		}
-	}
-
-	protected function writeIpLog($ip)
-	{
-		$post = $this->post;
-
-		/** @var \XF\Repository\IP $ipRepo */
-		$ipRepo = $this->repository('XF:Ip');
-		$ipEnt = $ipRepo->logIp($post->user_id, $ip, 'post', $post->post_id);
-		if ($ipEnt)
-		{
-			$post->fastUpdate('ip_id', $ipEnt->ip_id);
 		}
 	}
 }
