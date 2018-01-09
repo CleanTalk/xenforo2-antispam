0126626a0f7d029de6904b9168ec75c8e850f3db
davydovct <davydov@cleantalk.org>
2018-01-10 00:40:32 +0500

    Change server_root var to xenforo_root Improve js protection

--- CleanTalk/XF/Spam/Checker/CleanTalkUser.php
+++ CleanTalk/XF/Spam/Checker/CleanTalkUser.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace CleanTalk\XF\Spam\Checker;
-require_once $_SERVER['DOCUMENT_ROOT'].'/src/addons/CleanTalk/Cleantalk.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/src/addons/CleanTalk/CleantalkHelper.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/src/addons/CleanTalk/CleantalkRequest.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/src/addons/CleanTalk/CleantalkResponse.php';
+require_once \XF::getRootDirectory().'/src/addons/CleanTalk/Cleantalk.php';
+require_once \XF::getRootDirectory().'/src/addons/CleanTalk/CleantalkHelper.php';
+require_once \XF::getRootDirectory().'/src/addons/CleanTalk/CleantalkRequest.php';
+require_once \XF::getRootDirectory().'/src/addons/CleanTalk/CleantalkResponse.php';
 use Cleantalk\Cleantalk;
 use Cleantalk\CleantalkRequest;
 use Cleantalk\CleantalkResponse;
@@ -40,35 +40,34 @@ class CleanTalkUser extends \XF\Spam\Checker\AbstractProvider implements \XF\Spa
 
 	protected function isSpam(\XF\Entity\User $user, $extraParams)
 	{
-		$refferrer = null;
-        if (isset($_SERVER['HTTP_REFERER'])) {
-            $refferrer = htmlspecialchars((string) $_SERVER['HTTP_REFERER']);
-        }
-
-        $user_agent = null;
-        if (isset($_SERVER['HTTP_USER_AGENT'])) {
-            $user_agent = htmlspecialchars((string) $_SERVER['HTTP_USER_AGENT']);
-        }
-        $sender_info = array(
-            'REFFERRER' => $refferrer,
-            'post_url' => $refferrer,
-            'USER_AGENT' => $user_agent
-        );
         $js_on = 0;
         if (isset($_POST['ct_checkjs']) && $_POST['ct_checkjs'] == date("Y"))
             $js_on = 1;    
         $decision = null;   
-        $cookie_timestamp = (isset($_COOKIE['ct_timestamp']) ? $_COOKIE['ct_timestamp'] : 0);
-        $sender_info = json_encode($sender_info);   
+        $page_set_timestamp = (isset($_COOKIE['ct_ps_timestamp']) ? $_COOKIE['ct_ps_timestamp'] : 0);
+        $js_timezone = (isset($_COOKIE['ct_timezone']) ? $_COOKIE['ct_timezone'] : '');
+        $first_key_timestamp = (isset($_COOKIE['ct_fkp_timestamp']) ? $_COOKIE['ct_fkp_timestamp'] : '');
+        $pointer_data = (isset($_COOKIE['ct_pointer_data']) ? json_decode($_COOKIE['ct_pointer_data']) : '');
+        $sender_info = json_encode(
+            array(
+                'REFFERRER' => (isset($_SERVER['HTTP_REFERER']))?htmlspecialchars((string) $_SERVER['HTTP_REFERER']):null,
+                'post_url' => (isset($_SERVER['HTTP_REFERER']))?htmlspecialchars((string) $_SERVER['HTTP_REFERER']):null,
+                'USER_AGENT' => (isset($_SERVER['HTTP_USER_AGENT']))?htmlspecialchars((string) $_SERVER['HTTP_USER_AGENT']):null,
+                'js_timezone' => $js_timezone,
+                'mouse_cursor_positions' => $pointer_data,
+                'key_press_timestamp' => $first_key_timestamp,
+                'page_set_timestamp' => $page_set_timestamp
+            )
+        ); 
         $ct = new Cleantalk();
         $ct_request = new CleantalkRequest();
         $ct_request->auth_key = trim($this->app->options()->ct_apikey);
         $ct_request->sender_email = $user->email;
         $ct_request->sender_nickname = $user->username;
         $ct_request->sender_ip = $ct->cleantalk_get_real_ip();
-        $ct_request->agent = 'xenforo2-14';
+        $ct_request->agent = 'xenforo2-15';
         $ct_request->js_on = $js_on;
-        $ct_request->submit_time = time() - $cookie_timestamp;
+        $ct_request->submit_time = time() - $page_set_timestamp;
         $ct_request->sender_info = $sender_info;
         $ct->work_url = 'http://moderate.cleantalk.org';
         $ct->server_url = 'http://moderate.cleantalk.org';
