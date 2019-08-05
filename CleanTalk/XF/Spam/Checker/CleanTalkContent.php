0e98e586c365661e7a562f648b58189837dd974d
Glomberg <bazz@bk.ru>
2019-08-05 15:46:56 +0300

    Fix: native stop-words integrated

--- CleanTalk/XF/Spam/Checker/CleanTalkContent.php
+++ CleanTalk/XF/Spam/Checker/CleanTalkContent.php
@@ -11,6 +11,8 @@ use CleanTalk\CleantalkRequest;
 use CleanTalk\CleantalkResponse;
 use CleanTalk\CleantalkHelper;
 
+use XF\Util\Arr;
+
 class CleanTalkContent extends \XF\Spam\Checker\AbstractProvider implements \XF\Spam\Checker\ContentCheckerInterface
 {
     protected function getType()
@@ -20,7 +22,52 @@ class CleanTalkContent extends \XF\Spam\Checker\AbstractProvider implements \XF\
 
     public function check(\XF\Entity\User $user, $message, array $extraParams = [])
     {
+        
+		$option = $this->app()->options()->spamPhrases;
+	    $phrases = Arr::stringToArray($option['phrases'], '/\r?\n/');
+
         $decision = 'allowed';
+
+	    foreach ($phrases AS $phrase)
+	    {
+		    $phrase = trim($phrase);
+		    if (!strlen($phrase))
+		    {
+			    continue;
+		    }
+
+		    $origPhrase = $phrase;
+
+		    if ($phrase[0] != '/')
+		    {
+			    $phrase = preg_quote($phrase, '#');
+			    $phrase = str_replace('\\*', '[\w"\'/ \t]*', $phrase);
+			    $phrase = '#(?<=\W|^)(' . $phrase . ')(?=\W|$)#iu';
+		    }
+		    else
+		    {
+			    if (preg_match('/\W[\s\w]*e[\s\w]*$/', $phrase))
+			    {
+				    // can't run a /e regex
+				    continue;
+			    }
+		    }
+
+		    try
+		    {
+			    if (preg_match($phrase, $message))
+			    {
+				    $decision = $option['action'] == 'moderate' ? 'moderated' : 'denied';
+
+				    $this->logDetail('spam_phrase_matched_x', [
+					    'phrase' => $origPhrase
+				    ]);
+
+				    break;
+			    }
+		    }
+		    catch (\ErrorException $e) {}
+	    }
 
         try
         {
