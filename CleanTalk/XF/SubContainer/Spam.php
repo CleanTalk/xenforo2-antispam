b7a44add43ecd17269a957cb7e01668e9d99c712
davydovct <davydovct@cleantalk.org>
2020-04-21 09:14:56 +0500

    Fix spam content container

--- CleanTalk/XF/SubContainer/Spam.php
+++ CleanTalk/XF/SubContainer/Spam.php
@@ -22,12 +22,12 @@ class Spam extends XFCP_Spam
 			},false);			
 		}
 
-		if ($this->app->options()->ct_checkcontent)
+		if ($this->app->options()->ct_checkcontent || $this->app->options()->ct_check_pm)
 		{
 			$container->set('contentProviders', function(Container $c) use ($parent)
 			{
 				$providers[] = 'CleanTalk\XF\Spam\Checker\CleanTalkContent';
-				$this->app->fire('spam_user_providers', [$this, $parent, &$providers]);
+				$this->app->fire('spam_content_providers', [$this, $parent, &$providers]);
 				return $providers;			
 			}, false);			
 		}
