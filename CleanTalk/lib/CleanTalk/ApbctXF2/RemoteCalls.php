2b3ce488f09e0187081c6a9133855d1c42170077
Artem Davydov <yozzeebeatz@gmail.com>
2021-05-03 16:05:17 +0500

    Fix: update lib folder name.

---
+++ CleanTalk/lib/CleanTalk/ApbctXF2/RemoteCalls.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace CleanTalk\ApbctXF2;
+
+class RemoteCalls extends \CleanTalk\Common\RemoteCalls {
+    /**
+     * SFW update
+     *
+     * @return string
+     */
+    public function action__sfw_update()
+    {
+        return \CleanTalk\ApbctXF2\Funcs::apbct_sfw_update( $this->api_key );
+    }
+
+    /**
+     * SFW send logs
+     *
+     * @return string
+     */
+    public function action__sfw_send_logs()
+    {
+        return \CleanTalk\ApbctXF2\Funcs::apbct_sfw_send_logs( $this->api_key );
+    }
+
+    public function action__sfw_update__write_base()
+    {
+        return \CleanTalk\ApbctXF2\Funcs::apbct_sfw_update( $this->api_key );
+    }
+    /**
+     * Get available remote calls from the storage.
+     *
+     * @return array
+     */
+    protected function getAvailableRcActions()
+    {
+        $remote_calls = json_decode(\CleanTalk\ApbctXF2\Funcs::getXF()->options()->ct_remote_calls, true);
+        $default_rc = array('close_renew_banner' => array('last_call' => 0, 'cooldown' => self::COOLDOWN), 'sfw_update' => array('last_call' => 0, 'cooldown' => self::COOLDOWN), 'sfw_send_logs' => array('last_call' => 0, 'cooldown' => self::COOLDOWN), 'sfw_update__write_base' => array('last_call' => 0, 'cooldown' => 0));
+        if ($remote_calls && !empty($remote_calls)) {
+            return empty(array_diff_key($remote_calls, $default_rc)) && is_array($remote_calls['sfw_update']) ? $remote_calls : $default_rc;
+        }
+        return $default_rc;
+    }
+
+    /**
+     * Set last call timestamp and save it to the storage.
+     *
+     * @param array $action
+     * @return void
+     */
+    protected function setLastCall( $action )
+    {
+        // TODO: Implement setLastCall() method.
+        $remote_calls = $this->getAvailableRcActions();
+        $remote_calls[$action]['last_call'] = time();
+        \CleanTalk\ApbctXF2\Funcs::getXF()->repository('XF:Option')->updateOption('ct_remote_calls', json_encode($remote_calls));
+    }
+}
